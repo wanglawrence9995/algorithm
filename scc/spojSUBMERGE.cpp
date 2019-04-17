c77d386d2bef9a562ee72773ec2978b08d27378b
wanglawrence9995 <wanglawrence9995@gmail.com>
2019-04-16 18:55:11 -0700

    submerge

--- scc/spojSUBMERGE.cpp
+++ scc/spojSUBMERGE.cpp
@@ -34,6 +34,63 @@ using vps = vector<ps>;
 #define sortv(v) sort(begin(v), end(v));
 
 auto out_iter(ostream_iterator<int>(std::cout, " "));
+
+//startsnippet:graph_articulation_points
+//https://github.com/stjepang/snippets/blob/master/articulation_points.cpp
+struct ArticulationPoints
+{
+    int n,m;
+    vii g;
+    int tick = 1;
+    vi discovery;
+    vb articulation;
+
+    int dfs(int x, int dad)
+    {
+        discovery[x] = ++tick;
+        int low = discovery[x];
+
+        articulation[x] = false;
+        int children = 0;
+
+        for (int y : g[x])
+        {
+            if (y == dad)
+                continue;
+
+            if (discovery[y] < 0)
+            {
+                ++children;
+                int w = dfs(y, x);
+
+                if (w >= discovery[x])
+                    articulation[x] = true;
+                low = min(low, w);
+            }
+            else
+            {
+                low = min(low, discovery[y]);
+            }
+        }
+        if (dad == -1)
+            articulation[x] = children > 1;
+
+        return low;
+    }
+    ArticulationPoints(int iN, int iM, vii &iG) : n(iN), m(iM), g(iG)
+    {
+        articulation.assign(n,false);
+        discovery.assign(n,-1);
+        forp(i,0,n){
+            if (discovery[i]==-1) dfs(i,-1);
+        }
+    }
+        //usage.
+        //  ArticulationPoints cps(n,m,gr.g);
+        // cout << count(cps.articulation.begin(), cps.articulation.end(), true)<< '\n';
+};
+//endsnippet:graph articulation points
+
 //startsnippet:graph_cutpoints
 struct CutPoints
 {
@@ -82,16 +139,22 @@ struct CutPoints
                 dfs(i);
         }
     }
+    //usage
+    // CutPoints cps(n, m, gr.g);
+    // cout << cps.cutpoints.size() << endl;
 };
 
 //endsnippet:cutpoints
+
+//startsnippet:input_graph_adj_undirected
 struct GraphAdjReader
 {
     vii g, gt;
     int n, m;
     int one = 1; //default to one based?
+    bool directed = false;
 
-    GraphAdjReader(int iN, int iM, bool oneBased) : n(iN), m(iM)
+    GraphAdjReader(int iN, int iM, bool oneBased, bool directed) : n(iN), m(iM), directed(directed)
     {
         g.assign(n, vi());
         gt.assign(n, vi());
@@ -124,29 +187,42 @@ istream &operator>>(istream &input, GraphAdjReader &item)
         ioline();
         is >> a >> b;
         item.g[a - one].emplace_back(b - one);
-        item.gt[b - one].emplace_back(a - one);
+        if (item.directed)
+        {
+            item.gt[b - one].emplace_back(a - one);
+        }
+        else
+        {
+            item.g[b - one].emplace_back(a - one);
+        }
     }
     return input;
 }
 
+//endsnippet:input graph reader directed undirected
+
 int main()
 {
-    freopen("spojSUBMERGE.01.inp","r",stdin);
+    // freopen("spojSUBMERGE.01.inp","r",stdin);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n,m;
+    int n, m;
     ioinit();
 
-    while(1){
+    while (1)
+    {
         ioline();
         is >> n >> m;
-        if (n==0 && m==0) return 0;
-        GraphAdjReader gr(n,m,true);
+        if (n == 0 && m == 0)
+            return 0;
+        GraphAdjReader gr(n, m, true, false);
         cin >> gr;
-        CutPoints cps(n,m, gr.g);
-        // cout << gr << endl;
-        // cout << endl;
-        cout << cps.cutpoints.size()<<endl;
+
+        // CutPoints cps(n, m, gr.g);
+        // cout << cps.cutpoints.size() << endl;
+
+        ArticulationPoints cps(n,m,gr.g);
+        cout << count(cps.articulation.begin(), cps.articulation.end(), true)<< '\n';
     }
     return 0;
 }
